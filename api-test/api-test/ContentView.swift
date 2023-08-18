6cf6f6b5a5813dc30a24ae9c8b733381db730ed5
Praful Maka <pmaka@Prafuls-MacBook-Pro.local>
2023-08-18 02:03:57 -0700

    tested API call

---
+++ api-test/api-test/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  api-test
+//
+//  Created by Praful Maka on 8/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+        .task {
+            do {
+                let resp = try await fetchFromAPI()
+                print(resp)
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
